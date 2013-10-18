f46bda400ca23bd50534828f8e5feb9d5bfe9f0a
Warren Buckley <warren@creativewebspecialist.co.uk>
2013-10-18 15:47:45 +0100

    Re-work Wizard to use native Nuget.Core API as allow to get Nuget dependancies as easier current way would require specifiying every single depenancy

--- Umbraco.VS.NewProject/Umbraco.VS.NewProject.Wizard/Wizard.cs
+++ Umbraco.VS.NewProject/Umbraco.VS.NewProject.Wizard/Wizard.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using Microsoft.VisualStudio.TemplateWizard;
 using System.Windows.Forms;
 using EnvDTE;
+using NuGet;
+using Umbraco.Core;
+using umbraco.presentation.install.utills;
 
 namespace Umbraco.VS.NewProject.Wizard
 {
     public class Wizard : IWizard
     {
         private UserInputForm inputForm;
-        private string dbConnectionString;
-        private string templateEnginge;
-        private string dbType;
+        private DTE _dte;
+
 
         /// <summary>
         /// Runs custom wizard logic at the beginning of a template wizard run.
@@ -22,24 +27,31 @@ namespace Umbraco.VS.NewProject.Wizard
         /// <param name="customParams">The custom parameters with which to perform parameter replacement in the project.</param>
         public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
         {
-            //MessageBox.Show("RunStarted()");
-        }
+            _dte = (DTE)automationObject;
 
+            Debug.Write("Umbraco New Project - RunStarted() event");
+        }
 
         /// <summary>
         /// Runs custom wizard logic when a project has finished generating.
         /// </summary>
         /// <param name="project">The project that finished generating.</param>
-        public void ProjectFinishedGenerating(EnvDTE.Project project)
+        public void ProjectFinishedGenerating(Project project)
         {
-            //MessageBox.Show("ProjectFinishedGenerating()");
+            Debug.Write("Umbraco New Project - ProjectFinishedGenerating() event");
 
             try
             {
-                var csProj = project.FileName;
+                var csProj          = project.FileName;
+                var projectPath     = Path.GetDirectoryName(csProj);
+                var solutionPath    = Path.GetDirectoryName(projectPath);
+
+                //Go Get Umbraco from Nuget
+                GetUmbraco(solutionPath, project);
 
                 // Display a form to the user. The form collects 
                 // input for the custom message.
+                /*
                 inputForm = new UserInputForm();
 
                 //Set a value on inputForm
@@ -47,10 +59,8 @@ namespace Umbraco.VS.NewProject.Wizard
 
                 //Show the form
                 inputForm.ShowDialog();
+                */
 
-                //Fetch value from input form
-                dbConnectionString  = inputForm.get_dbConnectionString();
-                dbType              = inputForm.get_dbType();
             }
             catch (Exception ex)
             {
@@ -65,7 +75,7 @@ namespace Umbraco.VS.NewProject.Wizard
         /// <param name="projectItem">The project item that finished generating.</param>
         public void ProjectItemFinishedGenerating(ProjectItem projectItem)
         {
-            //MessageBox.Show("ProjectItemFinishedGenerating()");
+            Debug.Write("Umbraco New Project - ProjectItemFinishedGenerating() event");
         }
 
 
@@ -88,7 +98,7 @@ namespace Umbraco.VS.NewProject.Wizard
         /// <param name="projectItem">The project item that will be opened.</param>
         public void BeforeOpeningFile(ProjectItem projectItem)
         {
-            //MessageBox.Show("BeforeOpeningFile()");
+            Debug.Write("Umbraco New Project - BeforeOpeningFile() event");
         }
 
         /// <summary>
@@ -96,7 +106,103 @@ namespace Umbraco.VS.NewProject.Wizard
         /// </summary>
         public void RunFinished()
         {
-            //MessageBox.Show("Run Finished()");
+            Debug.Write("Umbraco New Project - RunFinished() event");
+        }
+
+
+        public void GetUmbraco(string solutionPath, Project project)
+        {
+            //Update IDE status bar bottom left
+            _dte.StatusBar.Text = "Umbraco New Project - Getting Umbraco (Please Wait)";
+
+            //Debug
+            Debug.Write("Umbraco New Project - GetUmbraco() event");
+            Debug.Write("FilePath: " + solutionPath);
+
+            //Get Packages folder at solution level
+            var packagePath = Path.Combine(solutionPath, "packages");
+
+
+            //ID of the package to be looked up
+            string packageID = "UmbracoCMS";
+
+            //Connect to the official package repository
+            IPackageRepository repo = PackageRepositoryFactory.Default.CreateRepository("https://packages.nuget.org/api/v2");
+
+            //Get the list of all NuGet packages with ID 'UmbracoCMS'       
+            List<IPackage> packages = repo.FindPackagesById(packageID).ToList();
+
+            //Get the latest Umbraco Nuget but only release (not pre-release, alpha stuff)
+            var umbracoNuget = packages.SingleOrDefault(x => x.IsLatestVersion && x.IsReleaseVersion());
+
+            //Initialize the package manager
+            var packageManager = new PackageManager(repo, solutionPath);
+
+            //Hook up Nuget events
+            packageManager.PackageInstalled += packageManager_PackageInstalled;
+            packageManager.PackageInstalling += packageManager_PackageInstalling;
+
+            //Download and unzip the package/s
+            //Gets all the dependcies needed as well
+            if (umbracoNuget != null)
+            {
+                packageManager.InstallPackage(umbracoNuget, false, false);
+            }
+        }
+
+        /// <summary>
+        /// Package Installing from Nuget event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void packageManager_PackageInstalling(object sender, PackageOperationEventArgs e)
+        {
+            var package = e.Package;
+            Debug.WriteLine("Umbraco New Project - PackageInstalling() event");
+            Debug.WriteLine(string.Format("Installing {0} {1}", package.Title, package.Version.ToString()));
+            Debug.WriteLine(string.Empty);
+
+            //Update IDE status bar bottom left
+            _dte.StatusBar.Text = string.Format("Installing {0} {1}", package.Title, package.Version.ToString());
+        }
+
+        /// <summary>
+        /// Package Installed from Nuget event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void packageManager_PackageInstalled(object sender, PackageOperationEventArgs e)
+        {
+            var package     = e.Package;
+            var fileSystem  = e.FileSystem;
+            var installPath = e.InstallPath;
+            var targetPath  = e.TargetPath;
+
+            var packageContentFiles = e.Package.GetContentFiles();
+            var packageFiles        = e.Package.GetFiles();
+
+            Debug.WriteLine("Umbraco New Project - PackageInstalled() event");
+            Debug.WriteLine(string.Format("Installed {0} {1}", package.Title, package.Version.ToString()));
+            Debug.WriteLine(string.Empty);
+
+            /*
+            string localSource = Path.Combine(targetPath, "packages");
+
+            IPackageRepository sourceRepo       =  PackageRepositoryFactory.Default.CreateRepository(new PackageSource("https://packages.nuget.org/api/v2"));
+            IPackagePathResolver pathResolver   = new DefaultPackagePathResolver(localSource);
+            IProjectSystem project;
+            IPackageRepository localRepo        = PackageRepositoryFactory.Default.CreateRepository(new PackageSource(localSource));
+
+
+            //Add nuget into project
+            ProjectManager projectManager = new ProjectManager(sourceRepo, pathResolver, project, localRepo);
+
+            if (projectManager != null)
+            {
+                //Add package to project
+                projectManager.AddPackageReference(package, true, false);  
+            }
+            */
         }
     }
 }
